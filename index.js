0a94783a681631cde63315e98926ddf6b7cf5c16
Aryankarma <110737632+Aryankarma@users.noreply.github.com>
2022-12-10 13:58:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var randomNumber1 = Math.floor((Math.random()*6)+1);
 var randomNumber2 = Math.floor((Math.random()*6)+1);
-var imageSource1 = "images/dice" + randomNumber1 + ".png";
-var imageSource2 = "images/dice" + randomNumber2 + ".png";
+var imageSource1 = "dice" + randomNumber1 + ".png";
+var imageSource2 = "dice" + randomNumber2 + ".png";
 document.querySelector(".img1").setAttribute("src", imageSource1);
 document.querySelector(".img2").setAttribute("src", imageSource2);
 if(randomNumber1 > randomNumber2){
